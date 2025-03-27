f64e130ad8cf3b6e6da81dedf9531464a70d13d4
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-27 19:42:22 -0400

    Added index action

--- app/Controllers/VendordashboardController.php
+++ app/Controllers/VendordashboardController.php
@@ -14,4 +14,8 @@ class VendordashboardController extends Controller {
     public function onConstruct(): void{
         $this->view->setLayout('default');
     }
+
+    public function indexAction() {
+        $this->view->render('vendordashboard/index');
+    }
 }
