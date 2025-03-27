4fcb262731adcfb8f86ad61ea4f6f4760d0df269
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-27 19:42:39 -0400

    Initial commit

---
+++ resources/views/vendordashboard/index.php
@@ -0,0 +1,12 @@
+<?php $this->setSiteTitle("Vendor Dashboard"); ?>
+
+<!-- Head content between these two function calls.  Remove if not needed. -->
+<?php $this->start('head'); ?>
+
+<?php $this->end(); ?>
+
+
+<!-- Body content between these two function calls. -->
+<?php $this->start('body'); ?>
+Vendor dashboard
+<?php $this->end(); ?>
