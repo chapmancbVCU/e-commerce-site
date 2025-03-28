3494e774ae7c14cc7cafe1dffc7ac25ca6367275
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-28 19:16:22 -0400

    Made responsive

--- resources/views/components/product_form.php
+++ resources/views/components/product_form.php
@@ -3,8 +3,10 @@
 <form class="form" action=<?=$this->postAction?> method="post" enctype="multipart/form-data">
     <?= FormHelper::csrfInput() ?>
     <?= FormHelper::displayErrors($this->displayErrors) ?>
-    <?= FormHelper::inputBlock('text', 'Name', 'name', '', ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-6']) ?>
-    <?= FormHelper::inputBlock('text', 'Price', 'price', '', ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
-    <?= FormHelper::inputBlock('text', 'List Price', 'list', '', ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
-    <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', '', ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
+    <div class="row g-3">
+        <?= FormHelper::inputBlock('text', 'Name', 'name', $this->product->name, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-6']) ?>
+        <?= FormHelper::inputBlock('text', 'Price', 'price', $this->product->price, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
+        <?= FormHelper::inputBlock('text', 'List Price', 'list', $this->product->list, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
+        <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', $this->product->shipping, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
+    </div>
 </form>
--- resources/views/vendorproducts/add.php
+++ resources/views/vendorproducts/add.php
@@ -9,8 +9,8 @@
 <!-- Body content between these two function calls. -->
 <?php $this->start('body'); ?>
 <h1 class="text-center">Add New Product</h1>
-<div class="row">
-    <div class="col-md-10 offset-md-1 bg-light">
+<div class="row g-3">
+    <div class="col-md-10 offset-md-1 bg-light p-4 rounded shadow-sm">
         <?php $this->component('product_form') ?>
     </div>
 </div>
