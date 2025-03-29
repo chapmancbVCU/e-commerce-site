3254f6f83651adc3577aec493737639d4c8420b7
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-28 21:28:08 -0400

    Moved displayErrors to each field and removed error bag and added description field

--- resources/views/components/product_form.php
+++ resources/views/components/product_form.php
@@ -2,13 +2,21 @@
 
 <form class="form" action=<?=$this->postAction?> method="post" enctype="multipart/form-data">
     <?= FormHelper::csrfInput() ?>
-    <?= FormHelper::displayErrors($this->displayErrors) ?>
     <div class="row g-3">
-        <?= FormHelper::inputBlock('text', 'Name', 'name', $this->product->name, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-6']) ?>
-        <?= FormHelper::inputBlock('text', 'Price', 'price', $this->product->price, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
-        <?= FormHelper::inputBlock('text', 'List Price', 'list', $this->product->list, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
-        <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', $this->product->shipping, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
+        <?= FormHelper::inputBlock('text', 'Name', 'name', $this->product->name, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-6'], $this->displayErrors) ?>
+        <?= FormHelper::inputBlock('text', 'Price', 'price', $this->product->price, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
+        <?= FormHelper::inputBlock('text', 'List Price', 'list', $this->product->list, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
+        <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', $this->product->shipping, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
     </div>
 
+    <?= FormHelper::textareaBlock(
+        'Description',
+        'description',
+        $this->product->description,
+        ['class' => 'form-control', 'rows' => '6'],
+        ['class' => 'form-group mt-3'],
+        $this->displayErrors
+    ) ?>
+    
     <?= FormHelper::submitBlock('Save', ['class' => 'btn btn-large btn-primary mt-3'], ['class' => 'text-end col-md-12']); ?>
 </form>
