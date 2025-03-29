21b5d57149dd7ce0e10176675840549c51266ab9
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-29 13:33:29 -0400

    Fixed indent

--- resources/views/components/product_form.php
+++ resources/views/components/product_form.php
@@ -1,27 +1,27 @@
 <?php use Core\FormHelper; ?>
 
-    <?= FormHelper::csrfInput() ?>
-    <div class="row g-3">
-        <?= FormHelper::inputBlock('text', 'Name', 'name', $this->product->name, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-6'], $this->displayErrors) ?>
-        <?= FormHelper::inputBlock('text', 'Price', 'price', $this->product->price, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
-        <?= FormHelper::inputBlock('text', 'List Price', 'list', $this->product->list, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
-        <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', $this->product->shipping, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
-    </div>
+<?= FormHelper::csrfInput() ?>
+<div class="row g-3">
+    <?= FormHelper::inputBlock('text', 'Name', 'name', $this->product->name, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-6'], $this->displayErrors) ?>
+    <?= FormHelper::inputBlock('text', 'Price', 'price', $this->product->price, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
+    <?= FormHelper::inputBlock('text', 'List Price', 'list', $this->product->list, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
+    <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', $this->product->shipping, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'], $this->displayErrors) ?>
+</div>
 
-    <?= FormHelper::textareaBlock(
-        'Description',
-        'description',
-        $this->product->description,
-        ['class' => 'form-control', 'rows' => '6'],
-        ['class' => 'form-group my-3'],
-        $this->displayErrors
-    ) ?>
+<?= FormHelper::textareaBlock(
+    'Description',
+    'description',
+    $this->product->description,
+    ['class' => 'form-control', 'rows' => '6'],
+    ['class' => 'form-group my-3'],
+    $this->displayErrors
+) ?>
 
-    <?= FormHelper::inputBlock('file', 
-        "Upload Product Image(s)", 
-        'productImages', 
-        '', 
-        ['multiple' => 'multiple', 'class' => 'form-control', 'accept' => 'image/gif image/jpeg image/png'], 
-        ['class' => 'form-group mb-3'], $this->displayErrors) 
-    ?>
+<?= FormHelper::inputBlock('file', 
+    "Upload Product Image(s)", 
+    'productImages', 
+    '', 
+    ['multiple' => 'multiple', 'class' => 'form-control', 'accept' => 'image/gif image/jpeg image/png'], 
+    ['class' => 'form-group mb-3'], $this->displayErrors) 
+?>
     
