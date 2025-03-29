2dcee32c451c136b08c4817949d99987218414ca
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-28 21:36:25 -0400

    Added back license_key

--- resources/js/descriptionTinyMCE.js
+++ resources/js/descriptionTinyMCE.js
@@ -4,4 +4,5 @@ tinymce.init({
   menubar : false,
   plugins: 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount',
   toolbar: 'undo redo | bold italic backcolor underline strikethrough | outdent indent | alignleft aligncenter alignright alignjustify | removeformat | help',
+  license_key: 'gpl'
 });
