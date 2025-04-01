1e21d618f81920ccbc7062c96ddf5a39e41358b8
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-31 20:50:04 -0400

    Added save and delete actions

--- app/Controllers/AdminbrandsController.php
+++ app/Controllers/AdminbrandsController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controllers;
 use Core\Controller;
+use App\Models\Users;
+use App\Models\Brands;
+use Core\Router;
 
 /**
  * Undocumented class
@@ -12,10 +15,44 @@ class AdminbrandsController extends Controller {
      * @return void
      */
     public function onConstruct(): void {
-        $this->view->setLayout('default');
+        $this->view->setLayout('vendoradmin');
+        $this->user = Users::currentUser();
     }
 
     public function indexAction() {
+        $brand = new Brands();
+
+        $this->view->brand = $brand;
+        $this->view->formErrors = $brand->getErrorMessages();
+        $this->view->brands = Brands::find(['order' => 'name']);
         $this->view->render('adminbrands/index');
+    }
+
+    public function saveAction() {
+        if($this->request->isPost()) {
+            $this->request->csrfCheck();
+            $brand = new Brands();
+            $brand->name = $this->request->get('name');
+            if($brand->save()) {
+                $resp = ['success' => true, 'brand' => $brand->data()];
+            } else {
+                $resp = ['success' => false, 'errors' => $brand->getErrorMessages()];
+            }
+            $this->jsonResponse($resp);
+        }
+    }
+
+    public function deleteAction(): void {
+        if ($this->request->isPost()) {
+            $this->request->csrfCheck();
+    
+            $id = $this->request->get('id');
+            $brand = Brands::findById($id);
+    
+            if ($brand) {
+                $brand->delete();
+            }
+        }
+        Router::redirect('adminbrands/index');
     }
 }
