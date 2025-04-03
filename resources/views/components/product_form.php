1063c13a08bd00c1d1e57c175a75273eda98ea5c
Chad Chapman <chad.chapman2010@gmail.com>
2025-04-02 20:42:48 -0400

    Adjusted with for shipping

--- resources/views/components/product_form.php
+++ resources/views/components/product_form.php
@@ -6,7 +6,7 @@
     <?= FormHelper::inputBlock('text', 'Name', 'name', $this->product->name, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-6']) ?>
     <?= FormHelper::inputBlock('text', 'Price', 'price', $this->product->price, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
     <?= FormHelper::inputBlock('text', 'List Price', 'list', $this->product->list, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2'],) ?>
-    <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', $this->product->shipping, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-2']) ?>
+    <?= FormHelper::inputBlock('text', 'Shipping', 'shipping', $this->product->shipping, ['class' => 'form-control input-sm'], ['class' => 'form-group col-md-3']) ?>
     <?= FormHelper::selectBlock('Brand', 'brand_id', $this->product->brand_id, $this->brands, ['class' => 'form-control input-sm', 'required' => 'required'], ['class' => 'form-group col-md-2']); ?>
 </div>
 
