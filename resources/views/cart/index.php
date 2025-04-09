8165ba8ec111cea2314dec86026e07d13efa2122
Chad Chapman <chad.chapman2010@gmail.com>
2025-04-09 19:05:17 -0400

    Initial commit

---
+++ resources/views/cart/index.php
@@ -0,0 +1,12 @@
+<?php $this->setSiteTitle("My title here"); ?>
+
+<!-- Head content between these two function calls.  Remove if not needed. -->
+<?php $this->start('head'); ?>
+
+<?php $this->end(); ?>
+
+
+<!-- Body content between these two function calls. -->
+<?php $this->start('body'); ?>
+
+<?php $this->end(); ?>
