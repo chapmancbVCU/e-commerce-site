fb139f0f3025820e4a60bf6f5f247b35eb4f7993
Chad Chapman <chad.chapman2010@gmail.com>
2025-04-16 21:13:21 -0400

    #Added buttons directing user to home or cart

--- resources/views/cart/addToCart.php
+++ resources/views/cart/addToCart.php
@@ -1,3 +1,4 @@
+<?php use Core\Lib\Utilities\Env; ?>
 <?php $this->setSiteTitle("My title here"); ?>
 
 <!-- Head content between these two function calls.  Remove if not needed. -->
@@ -8,5 +9,12 @@
 
 <!-- Body content between these two function calls. -->
 <?php $this->start('body'); ?>
-links to continue shopping or go to cart
+
+<div class="row">
+    <div class="col col-md-8 offset-md-2 text-center">
+        <h3>Do you want to check out or continue shopping?</h3>
+        <a href="<?=Env::get('APP_DOMAIN')?>" class="btn btn-lg btn-info">Continue Shopping</a>
+        <a href="<?=Env::get('APP_DOMAIN')?>cart" class="btn btn-lg btn-primary">Checkout</a>
+    </div>
+</div>
 <?php $this->end(); ?>
