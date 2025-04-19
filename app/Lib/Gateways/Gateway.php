909cba9c35643746bce170332298ca4bb2ca82b1
Chad Chapman <chad.chapman2010@gmail.com>
2025-04-19 15:33:26 -0400

    Implemented class with build function

--- app/Lib/Gateways/Gateway.php
+++ app/Lib/Gateways/Gateway.php
@@ -1,2 +1,13 @@
 <?php
 
+namespace App\Lib\Gateways;
+use App\Lib\Gateways\{StripeGateway};
+use Core\Lib\Utilities\Env;
+
+class Gateway {
+    public static function build($cart_id) {
+        if(Env::get('GATEWAY') == 'stripe') {
+            return new StripeGateway($cart_id);
+        }
+    }
+}
