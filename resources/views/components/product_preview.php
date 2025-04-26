ffc52cecf4233d691964f0465fb077ceed26c5f3
Chad Chapman <chad.chapman2010@gmail.com>
2025-04-26 13:56:33 -0400

    Implemented content for product preview panel

--- resources/views/components/product_preview.php
+++ resources/views/components/product_preview.php
@@ -1,1 +1,19 @@
-test
+<?php use Core\Lib\Utilities\Env; ?>
+<h3>Purchase Summary</h3>
+<?php foreach($this->items as $item): ?>
+    <div class="cart-preview-item">
+        <div class="cart-preview-item-image">
+            <img src="<?=Env::get('APP_DOMAIN') . $item->url?>" alt="<?=$item->name?>">
+        </div>
+        <div class="cart-preview-item-info">
+            <p><?=$item->name?></p>
+            <p><?=$item->qty?> @ $<?=$item->price?></p>
+            <p>Shipping: $<?=$item->shipping?></p>
+        </div>
+    </div>
+<?php endforeach; ?>
+
+<div class="d-flex justify-content-between">
+    <div class="text-bold">Total:</div>
+    <div class="text-bold">$<?=number_format($this->grandTotal, 2)?></div>
+</div>
