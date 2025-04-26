0ca3f550748f05f11b8be69f0a170df80dd85d87
Chad Chapman <chad.chapman2010@gmail.com>
2025-04-26 18:12:31 -0400

    Implemented remainder of functions and resolved some errors

--- app/Lib/Gateways/StripeGateway.php
+++ app/Lib/Gateways/StripeGateway.php
@@ -4,9 +4,12 @@ namespace App\Lib\Gateways;
 use Stripe\Charge;
 use Stripe\Stripe;
 use Core\Lib\Utilities\Env;
+use App\Models\Transactions;
 use App\Lib\Gateways\AbstractGateway;
 
 class StripeGateway extends AbstractGateway {
+    public static $gateway = 'stripe';
+
     public function getView() {
         return 'card_forms/stripe';
     }
@@ -16,10 +19,19 @@ class StripeGateway extends AbstractGateway {
             'amount' => $this->grandTotal * 100,
             'currency' => 'usd',
             'description' => 'Chappy.php Purchase: ' . $this->item_count .  'items. Cart ID: ' . $this->cart_id,
-            'source' => $post['stripeToken']
+            'source' => $post['stripeToken'],
+            'shipping' => ['address' => $this->parseShippingAddress($post), 'name' => $post['name']]
         ];
 
         $ch = $this->charge($data);
+        $this->handleChargeResponse($ch);
+        $tx = $this->createTransaction($ch);
+        return [
+            'success' => $this->chargeSuccess,
+            'message' => $this->msgToUser,
+            'tx' => $tx,
+            'charge_id' => $ch->id
+        ];
     }
 
     public function charge($data) {
@@ -28,7 +40,40 @@ class StripeGateway extends AbstractGateway {
         return $charge;
     }
 
-    public function handleChargeResponse($ch) {}
+    public function handleChargeResponse($ch) {
+        $this->chargeSuccess = $ch->outcome->network_status == 'approved_by_network';
+        $this->msgToUser = $ch->outcome->seller_message;
+    }
 
-    public function createTransaction($ch) {}
+    protected function parseShippingAddress($post) {
+        return [
+            'line1' => $post['shipping_address1'],
+            'line2' => $post['shipping_address2'],
+            'city' => $post['shipping_city'],
+            'state' => $post['shipping_state'],
+            'postal_code' => $post['shipping_zip'],
+        ];
+    }
+
+    public function createTransaction($ch) {
+        $tx = new Transactions();
+        $tx->cart_id = $this->cart_id;
+        $tx->gateway = static::$gateway;
+        $tx->type = $ch->payment_method_details->type;
+        $tx->amount = $this->grandTotal;
+        $tx->success = ($this->chargeSuccess) ? 1 : 0;
+        $tx->charge_id = $ch->id;
+        $tx->reason = $ch->outcome->reason;
+        $tx->card_brand = $ch->payment_method_details->card->brand;
+        $tx->last4 = $ch->payment_method_details->card->last4;
+        $tx->name = $ch->shipping->name;
+        $tx->shipping_address1 = $ch->shipping->address->line1;
+        $tx->shipping_address2 = $ch->shipping->address->line2;
+        $tx->shipping_city = $ch->shipping->address->city;
+        $tx->shipping_state = $ch->shipping->address->state;
+        $tx->shipping_zip = $ch->shipping->address->postal_code;
+        $tx->shipping_country = $ch->shipping->address->country;
+        $tx->save();
+        return $tx;
+    }
 }
