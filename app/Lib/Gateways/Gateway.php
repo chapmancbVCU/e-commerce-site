1fb984d36a9b268d99ba891a4825938578e50658
Chad Chapman <chad.chapman2010@gmail.com>
2025-04-26 20:37:48 -0400

    Added BraintreeGateway to build

--- app/Lib/Gateways/Gateway.php
+++ app/Lib/Gateways/Gateway.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Lib\Gateways;
-use App\Lib\Gateways\{StripeGateway};
 use Core\Lib\Utilities\Env;
+use App\Lib\Gateways\{BraintreeGateway, StripeGateway};
 
 class Gateway {
     public static function build($cart_id) {
         if(Env::get('GATEWAY') == 'stripe') {
             return new StripeGateway($cart_id);
+        } else if(Env::get('GATEWAY') == 'braintree') {
+            return new BraintreeGateway($cart_id);
         }
     }
 }
