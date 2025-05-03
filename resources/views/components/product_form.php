72f932ffba805d7fefa076841a56a166b14bb8cf
Chad Chapman <chad.chapman2010@gmail.com>
2025-05-03 17:29:09 -0400

    Refactored form to be able to check if options are available

--- resources/views/components/product_form.php
+++ resources/views/components/product_form.php
@@ -10,20 +10,43 @@
     <?= FormHelper::selectBlock('Brand', 'brand_id', $this->product->brand_id, $this->brands, ['class' => 'form-control input-sm', 'required' => 'required'], ['class' => 'form-group col-md-3']); ?>
 </div>
 
-<?= FormHelper::textareaBlock(
-    'Description',
-    'description',
-    $this->product->description,
-    ['class' => 'form-control'],
-    ['class' => 'form-group my-3']) 
-?>
+<div class="row">
+    <?= FormHelper::textareaBlock(
+        'Description',
+        'description',
+        $this->product->description,
+        ['class' => 'form-control'],
+        ['class' => 'form-group my-3']) 
+    ?>
+    <?= FormHelper::checkboxBlockLabelLeft('Featured', 'featured', "on", $this->product->isChecked(), [], ['class' => 'form-group mt-3 mb-2']) ?>
+    <?= FormHelper::checkboxBlockLabelLeft('Has Options', 'has_options', "on", $this->product->hasOptions(), [], ['class' => 'form-group mt-2 mb-3']) ?>
+</div>
 
-<?= FormHelper::checkboxBlockLabelLeft('Featured', 'featured', "on", $this->product->isChecked(), [], ['class' => 'form-group my-3']) ?>
-<?= FormHelper::inputBlock('file', 
-    "Upload Product Image(s)", 
-    'productImages[]', 
-    '', 
-    ['multiple' => 'multiple', 'class' => 'form-control', 'accept' => 'image/gif image/jpeg image/png'], 
-    ['class' => 'form-group mb-3'])
-?>
+<div id="optionsWrapper" class="row mb-3 <?= ($this->product->hasOptions()?"d-block" : "d-none")?>">
+    <div class="col-12">
+        Put table and select search box here
+    </div>
+</div>
+
+<div class="row">
+    <?= FormHelper::inputBlock('file', 
+        "Upload Product Image(s)", 
+        'productImages[]', 
+        '', 
+        ['multiple' => 'multiple', 'class' => 'form-control', 'accept' => 'image/gif image/jpeg image/png'], 
+        ['class' => 'form-group mb-3'])
+    ?>
+</div>
     
+<script>
+    document.getElementById('has_options').addEventListener('change', function(evt) {
+        const wrapper = document.getElementById('optionsWrapper');
+        if(evt.target.checked) {
+            wrapper.classList.add('d-block');
+            wrapper.classList.remove('d-none');
+        } else {
+            wrapper.classList.add('d-none');
+            wrapper.classList.remove('d-block');
+        }
+    });
+</script>
